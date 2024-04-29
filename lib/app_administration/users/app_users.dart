6ad6431c66788f5cca3b98da735523fd3fb97a81
USER <shibilmuhammed787@gmail.com>
2024-04-29 16:52:29 +0530

    Stream added

--- lib/app_administration/users/app_users.dart
+++ lib/app_administration/users/app_users.dart
@@ -2,6 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:nectar_admin/colors.dart';
 
+import '../../main.dart';
+
 class appUsers extends StatefulWidget {
   const appUsers({super.key});
 
@@ -29,31 +31,60 @@ class _appUsersState extends State<appUsers> {
           color: theColors.primaryColor,fontWeight: FontWeight.w600
         ),),
       ),
-      body: Column(
-        children: [
-          StreamBuilder<QuerySnapshot>(
-              stream: FirebaseFirestore.instance.collection("accounts").snapshots(),
-              builder: (context, snapshot) {
-                if(!snapshot.hasData){
-                  return Center(
-                    child: Text("No Data Found"),
-                  );
-                }
-                var data=snapshot.data!.docs;
-                return data.length==0?
-                    Text("No User Found")
-                : ListView.builder(
-                  itemCount: data.length,
-                  physics: BouncingScrollPhysics(),
-                    scrollDirection: Axis.vertical,
-                    shrinkWrap: true,
-                    itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(data[index]["name"]),
-                    );
-                    },);
-              },)
-        ],
+      body: Padding(
+        padding: EdgeInsets.all(w*0.03),
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Column(
+            children: [
+              StreamBuilder<QuerySnapshot>(
+                  stream: FirebaseFirestore.instance.collection("account").snapshots(),
+                  builder: (context, snapshot) {
+                    if(!snapshot.hasData){
+                      return Center(
+                        child: Text("No Data Found"),
+                      );
+                    }
+                    var data=snapshot.data!.docs;
+                    return data.length==0?
+                        Text("No User Found")
+                    : ListView.builder(
+                      itemCount: data.length,
+                      physics: BouncingScrollPhysics(),
+                        scrollDirection: Axis.vertical,
+                        shrinkWrap: true,
+                        itemBuilder: (context, index) {
+                        return Container(
+                          height: h*0.15,
+                          width: w*1,
+                          padding: EdgeInsets.all(w*0.03),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(w*0.03),
+                            border: Border.all(
+                              color: theColors.secondary
+                            )
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(data[index]["name"]),
+                              Row(
+                                children: [
+                                  InkWell(
+                                    onTap: () {
+                                      
+                                    },
+                                      child: Icon(Icons.delete))
+                                ],
+                              )
+                            ],
+                          ),
+                        );
+                        },);
+                  },)
+            ],
+          ),
+        ),
       ),
     );
   }
