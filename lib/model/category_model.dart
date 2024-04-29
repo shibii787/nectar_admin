2787290f491293c563b85410a5631c848c608939
USER <shibilmuhammed787@gmail.com>
2024-04-29 17:15:06 +0530

    model added

---
+++ lib/model/category_model.dart
@@ -0,0 +1,42 @@
+class CategoryModel{
+  final String itemName;
+  final int price;
+  final int qty;
+  final String image;
+  CategoryModel({
+    required this.itemName,
+    required this.price,
+    required this.qty,
+    required this.image,
+});
+  Map<String,dynamic> toMap(){
+    return{
+      "ItemName" : this.itemName,
+      "price" : this.price,
+      "qty" : this.qty,
+      "image" : this.image,
+    };
+  }
+  factory CategoryModel.fromMap(Map<String,dynamic>map){
+    return CategoryModel(
+        itemName: map["itemName"] ?? "",
+        price: map["price"] ?? "",
+        qty: map["qty"] ?? "",
+        image: map["image"] ?? ""
+    );
+  }
+  CategoryModel copyWith({
+    String? itemName,
+    int? price,
+    int? qty,
+    String? image
+}){
+    return CategoryModel(
+        //name: name ?? this.name,
+        itemName: itemName ?? this.itemName,
+        price: price ?? this.price,
+        qty: qty ?? this.qty,
+        image: image ?? this.image
+    );
+  }
+}
