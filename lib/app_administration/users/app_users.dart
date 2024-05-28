61c5298fb63121e242f6b51b09a6442c270ca980
HP G5 <jithinkannadi2002@gmail.com>
2024-05-28 17:53:03 +0530

    add groceryusermodel

--- lib/app_administration/users/app_users.dart
+++ lib/app_administration/users/app_users.dart
@@ -1,24 +1,19 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nectar_admin/core/common/colors.dart';
-import 'package:nectar_admin/feature/controller/addingController.dart';
 
-import '../../main.dart';
-
-class appUsers extends ConsumerStatefulWidget {
+class appUsers extends StatefulWidget {
   const appUsers({super.key});
 
   @override
-  ConsumerState<appUsers> createState() => _appUsersState();
+  State<appUsers> createState() => _appUsersState();
 }
 
-class _appUsersState extends ConsumerState<appUsers> {
-
+class _appUsersState extends State<appUsers> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: theColors.primaryColor,
+      backgroundColor:theColors.primaryColor,
       appBar: AppBar(
         backgroundColor: theColors.third,
         automaticallyImplyLeading: false,
@@ -34,86 +29,38 @@ class _appUsersState extends ConsumerState<appUsers> {
           color: theColors.primaryColor,fontWeight: FontWeight.w600
         ),),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(w*0.03),
-        child: Column(
-          children: [
-            ref.watch(streamdataProvider).when(
-                data: (data) {
-                  return Expanded(
-                    child: GridView.builder(
-                      itemCount: data.length,
-                        physics: BouncingScrollPhysics(),
-                        shrinkWrap: true,
-                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          childAspectRatio: 1,
-                            crossAxisSpacing: w*0.01,
-                            mainAxisSpacing: w*0.01,
-                            crossAxisCount: 4),
-                        itemBuilder: (context, index) {
-                          return Container(
-                            height: h*0.8,
-                            width: w*0.8,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(w*0.03),
-                              color: Colors.red
-                            ),
-                            margin: EdgeInsets.all(w*0.02),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                Text(data[index].name),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                  children: [
-                                    ElevatedButton(onPressed: () {
-                                      showDialog(
-                                          context: context,
-                                          builder: (context) {
-                                            return AlertDialog(
-                                              title: Center(
-                                                  child: Text("User Details",style: TextStyle(
-                                                    fontWeight: FontWeight.w600
-                                                  ),)),
-                                              actions: [
-                                                Column(
-                                                  children: [
-                                                    Text("Name: ${data[index].name}"),
-                                                    Text("Email: ${data[index].email}"),
-                                                    Text("Password: ${data[index].password}"),
-                                                    Text("Location: ${data[index].location}"),
-                                                    Text("PhoneNumber: ${data[index].name}"),
-                                                    Text("ID: ${data[index].id}"),
-                                                  ],
-                                                )
-                                              ],
-                                            );
-                                          },);
-                                    }, child: Text("View")),
-
-                                    ElevatedButton(onPressed: () {
-                                      FirebaseFirestore.instance.collection("account").doc(data[index].id).delete();
-                                    }, child: Icon(Icons.delete_outline))
-                                  ],
-                                )
-                              ],
-                            ),
-                          );
-                        },),
-                  );
-                },
-                error: (error, stackTrace) {
-                  return Text(error.toString());
-                },
-                loading: () {
+      body: Column(
+        children: [
+          StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance.collection("account").snapshots(),
+              builder: (context, snapshot) {
+                if(!snapshot.hasData){
                   return Center(
-                      child: CircularProgressIndicator(),
+                    child: Text("No Data Found"),
+                  );
+                }
+                var data=snapshot.data!.docs;
+                return data.length==0?
+                    Text("No User Found")
+                : ListView.builder(
+                  itemCount: data.length,
+                  physics: BouncingScrollPhysics(),
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(data[index]["name"],style: TextStyle(
+                        color: Colors.black
+                      ),),
                     );
-                },)
-          ],
-        ),
+                    },);
+              },),
+
+        ],
+
       ),
+
     );
+
   }
 }
-
